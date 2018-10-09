2b153164dfa0b2ae7ba8e29c08bd51ef1bb8e85c
Mary Gansallo <marygans@microsoft.com>
2018-10-09 15:53:14 -0700

    height change for cross browser compatibility

--- src/components/FooterMain.tsx
+++ src/components/FooterMain.tsx
@@ -5,11 +5,11 @@ const FooterMain = () => {
 
     return (
          <div className="footer-links">
-              <a className="footer-links" href="mailto:ssedesign_coherence@microsoft.com">Contact Us</a>
+          <a className="footer-links" href="mailto:ssedesign_coherence@microsoft.com">Contact Us</a>
               <a className="footer-links" href="https://www.microsoft.com/en-us/legal/intellectualproperty/copyright/default.aspx" target="_blank">Terms of Use</a>
               <a className="footer-links" href="https://privacy.microsoft.com/en-US/privacystatement" target="_blank">Privacy and cookies</a>
               <a  className="footer-links" href="https://www.microsoft.com/en-us/legal/intellectualproperty/trademarks/en-us.aspx" target="_blank">Trademarks</a>
-              <p>&copy; Microsoft 2018</p>
+              <p>&copy; Microsoft 2018</p>             
 
             </div>
     );
